61740028510fec67f07fcce1ca42e0ad2e27657a
zwelhtetyan <zwelhtetyan2001@gmail.com>
2022-12-22 13:27:17 +0630

    finish 🫣

--- index.js
+++ index.js
@@ -9,18 +9,31 @@ const cardImgs = [
 1. insert Cards 🃏
 2. click play game and shuffle cards
 3. generate card randomly and insert again to dom
-4. theme
+4. choose card and calculate win / lose and restart new round
+5. total round is 3 and show game over message after final round
+6. theme
 */
 
-const insertCards = (cards, isFlip) => {
+// selecting elements before insert the cards
+const playGameBtn = document.querySelector('.play-game');
+const desc = document.querySelector('.desc');
+const scoreBadge = document.querySelector('.score-badge');
+const roundBadge = document.querySelector('.round-badge');
+
+// default variables
+const ACE_ID = 4;
+let SCORE = 0;
+let ROUND = 1;
+
+const insertCards = (cardsArr, isFlip) => {
   const cardContainer = document.querySelector('.card-container');
 
   const classes = isFlip ? ' flip' : '';
 
-  const html = cards
+  const html = cardsArr
     .map(
-      (img) => `<div class='card'>
-  <div class='card-inner${classes}'>
+      (img) => `<button disabled class='card'>
+  <div class='card-inner${classes}' id=${img.id}>
     <div class='card-front'>
       <img src=${img.imagePath} alt='' class='card-img' />
     </div>
@@ -28,22 +41,109 @@ const insertCards = (cards, isFlip) => {
       <img src='./images/card-back-Blue.png' alt='' class='card-img' />
     </div>
   </div>
-</div>`
+</button>`
     )
     .join('');
+
+  // insert to the DOM
   cardContainer.innerHTML = html;
+
+  const cards = document.querySelectorAll('.card');
+
+  isFlip &&
+    setTimeout(() => cards.forEach((card) => (card.disabled = false)), 4000);
+
+  isFlip && updateROUND(ROUND);
+
+  cards.forEach((card) =>
+    card.addEventListener('click', function () {
+      flipCard(cards, this);
+    })
+  );
 };
 
-// insert card
+// inserting card
 insertCards(cardImgs, false);
 
-// selecting elements
-const playGameBtn = document.querySelector('.play-game');
-const desc = document.querySelector('.desc');
-const cards = document.querySelectorAll('.card');
-const innerCards = document.querySelectorAll('.card-inner');
+const flipCard = (cards, card) => {
+  const cardInner = card.childNodes[1];
+  cardInner.classList.remove('flip');
+  cards.forEach((card) => (card.disabled = true));
+  chooseCard(cardInner.id);
+  flipAllCard(cards);
 
-// generateRandomNumber
+  if (ROUND <= 3) {
+    restartNewRound(); //restart new round
+  } else {
+    setTimeout(gameOverAction, 2500);
+    // gameOverAction(); //  game over
+  }
+};
+
+const flipAllCard = (cards) => {
+  const innerCardsToFlip = [...cards].filter((card) =>
+    card.childNodes[1].classList.contains('flip')
+  );
+
+  let idx = 0;
+
+  const INTERVAL = setInterval(() => {
+    innerCardsToFlip[idx].childNodes[1].classList.remove('flip');
+
+    idx++;
+
+    if (idx > innerCardsToFlip.length - 1) {
+      clearInterval(INTERVAL);
+    }
+  }, 700);
+};
+
+const restartNewRound = () => setTimeout(shuffleCard, 3000);
+
+const updateScore = (SCORE) => (scoreBadge.children[0].textContent = SCORE);
+
+const updateROUND = (ROUND) => (roundBadge.children[0].textContent = ROUND);
+
+const chooseCard = (id) => {
+  if (+id === ACE_ID) {
+    desc.textContent = 'Hit! - Well done 👏🎉';
+    desc.style.color = '#10d510';
+
+    SCORE += 10;
+    ROUND++;
+
+    updateScore(SCORE);
+  } else {
+    desc.textContent = 'Missed! - Nice try 🫡';
+    desc.style.color = 'red';
+
+    SCORE -= 3;
+    ROUND++;
+
+    updateScore(SCORE);
+  }
+};
+
+const changeTopBarContent = () => {
+  scoreBadge.style.display = 'block';
+  roundBadge.style.display = 'block';
+  scoreBadge.children[0].textContent = SCORE;
+  roundBadge.children[0].textContent = ROUND;
+  document.querySelector('.top-bar').style.justifyContent = 'space-between';
+  desc.textContent = 'Choose the card that you think is the ACE of Spades.';
+};
+
+const gameOverAction = () => {
+  scoreBadge.style.display = 'none';
+  roundBadge.style.display = 'none';
+  document.querySelector('.top-bar').style.justifyContent = 'center';
+  desc.innerHTML = `Game Over! Final score is <span class="badge">${SCORE}</span>`;
+  desc.style.color = 'var(--text-primary)';
+  playGameBtn.style.display = 'block';
+  playGameBtn.style.animationDelay = 'unset';
+  playGameBtn.textContent = 'Play Again';
+};
+
 const generateRandomNumber = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
@@ -52,9 +152,13 @@ const shuffleCard = () => {
   // hide play-game button and change description
   playGameBtn.style.display = 'none';
   desc.textContent = 'Shuffling...🃏';
+  desc.style.color = 'var(--text-primary)';
 
   const lefts = ['-10rem', '-8rem', '-6rem', '-4rem', '-2rem', '0rem', '2rem'];
   const tops = ['0rem', '2rem', '4rem', '6rem', '8rem', '10rem', '12rem'];
+
+  const innerCards = document.querySelectorAll('.card-inner');
+  const cards = document.querySelectorAll('.card');
 
   innerCards.forEach((card) => card.classList.add('flip'));
 
@@ -69,13 +173,6 @@ const shuffleCard = () => {
     });
   }, 100);
 
-  const changeTopBarContent = () => {
-    document.querySelector('.score-badge').style.display = 'block';
-    document.querySelector('.round-badge').style.display = 'block';
-    document.querySelector('.top-bar').style.justifyContent = 'space-between';
-    desc.textContent = 'Choose the card that you think is the ACE of Spades.';
-  };
-
   const randomCardArr = cardImgs.sort(() => Math.random() - 0.5);
 
   setTimeout(() => {
@@ -86,7 +183,14 @@ const shuffleCard = () => {
 };
 
 // start the game
-playGameBtn.addEventListener('click', shuffleCard);
+playGameBtn.addEventListener('click', function () {
+  shuffleCard();
+
+  if (this.textContent === 'Play Again') {
+    SCORE = 0;
+    ROUND = 1;
+  }
+});
 
 // theme
 const moon = document.querySelector('.moon');
